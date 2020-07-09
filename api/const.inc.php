bda56503fe41a496472975fab23c38abfe40a43c
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-09 01:10:37 -0500

    Shifted constant defs around to organize better. Added const errors for SignUp errors

--- api/const.inc.php
+++ api/const.inc.php
@@ -1,9 +1,19 @@
 <?php
 //Success Codes
-define("LOGINSUCCESS", "HTTP/1.1 200 Successful Login");
+define("OPSUCCESS", "HTTP/1.1 200 Successful Operation");
 
-//Error Codes
-define("EMPTYFIELDS", "HTTP/1.1 432 Empty Input Fields");
+//Global Error Codes
 define("SQLERROR", "HTTP/1.1 439 SQL Connection Error");
-define("PASSINCORRECT", "HTTP/1.1 433 Password Incorrect");
+define("NOTALLOWED", "HTTP/1.1 438 Access Not Allowed");
+define("EMPTYFIELDS", "HTTP/1.1 432 Empty Input Fields");
+
+//Login Specific Error Codes
+define("PASSINCORRECT", "HTTP/1.1 433 Incorrect Passwords");
 define("USERINCORRECT", "HTTP/1.1 434 User Not Found");
+
+//Sign-Up Specific Error Codes
+define("INVALIDUIDEMAIL", "HTTP/1.1 445 Email Address & Username Invalid");
+define("INVALIDEMAIL", "HTTP/1.1 446 Email Address Invalid");
+define("INVALIDUID", "HTTP/1.1 447 Username Invalid (No Special Characters)");
+define("INVALIDPWDCHECK", "HTTP/1.1 448 Passwords Don't Match");
+define("USERTAKEN", "HTTP/1.1 449 Username Already Taken");
