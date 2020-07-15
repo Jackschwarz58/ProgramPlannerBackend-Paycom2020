0724af4a370df869d9d5b49ec18bf266916eb009
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-14 20:57:03 -0500

    Added logout functionality and API calls

---
+++ api/logout.php
@@ -0,0 +1,31 @@
+<?php
+require 'const.inc.php';
+
+if (isset($_COOKIE['login_usr_id'])) {
+
+    setcookie('login_usr_name', "", [
+        'expires' => time() - 3600,
+        'path' => '/',
+        'secure' => false,
+        'httponly' => false,
+        'samesite' => 'Lax',
+    ]);
+    setcookie('login_usr_email', "", [
+        'expires' => time() - 3600,
+        'path' => '/',
+        'secure' => false,
+        'httponly' => false,
+        'samesite' => 'Lax',
+    ]);
+    setcookie('login_usr_id', "", [
+        'expires' => time() - 3600,
+        'path' => '/',
+        'secure' => false,
+        'httponly' => false,
+        'samesite' => 'Lax',
+    ]);
+
+    header(COOKIESDELETED);
+} else {
+    header(COOKIEERROR);
+}
